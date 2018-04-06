2347cdf9ff88c0b9c572801c418175a156b199d8
Cntt04 <Admin04@Cntt04s-iMac.local>
2018-04-06 10:41:48 +0700

    Initial Commit

---
+++ appquizz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  appquizz
+//
+//  Created by Cntt04 on 4/6/18.
+//  Copyright © 2018 Cntt04. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
